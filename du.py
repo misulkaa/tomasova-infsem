10e74e18b5ecd67fa081f5ce4060c8f2af670111
[misulkaa] <misullka@michaelas-MacBook-Air.local>
2025-09-30 00:33:43 +0300

    du

--- du.py
+++ du.py
@@ -1,5 +1,15 @@
 #Napíš program, vypíše, ak súčin dvoch ľubovoľných čísel “presiahol 100”  a ak nepresiahol, vypíše “nepresiiahol 100”
-print("hello world")
-print ("hello world")
-print("hello world")
-print("hello world")
+
+x = float(input("napis cinitela  "))
+y = float(input("zapis druheho cinitela "))
+
+z = x * y
+
+if z < 100:
+    print( z, "nepriesiahol 100")
+else:
+    print( z, "presiahlo 100")
+
+
+
+
