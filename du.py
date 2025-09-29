36c16b858b10db294fbe5b87e63479718afa1a4a
[misulkaa] <misullka@michaelas-MacBook-Air.local>
2025-09-29 18:51:40 +0300

    du

--- du.py
+++ du.py
@@ -1,2 +1,5 @@
 #Napíš program, vypíše, ak súčin dvoch ľubovoľných čísel “presiahol 100”  a ak nepresiahol, vypíše “nepresiiahol 100”
 print("hello world")
+print ("hello world")
+print("hello world")
+print("hello world")
