14792e82cc498b1bbc931cf61b6522bf2fde6fb9
[misulkaa] <misullka@michaelas-MacBook-Air.local>
2025-09-30 00:50:39 +0300

    9

---
+++ 9.py
@@ -0,0 +1,9 @@
+#Napíš program, kt. zistí, či je v danom reťazci dané písmeno.
+
+x = input(" zadaj retazec ")
+y = input(" yadaj pismeno  ")
+
+if y in x:
+    print("v retazci sa nachadza dane pismeno")
+else:
+    print(" v retazci sa dane pismeno nenachadza")
